c163fc21c4ff0e51ecfbc46c1905828c19db9568
demo_sx <23699812+CN-COTER@users.noreply.github.com>
2020-08-23 11:40:56 +0800

    56 合并区间 线性排序

---
+++ 56.合并区间.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=56 lang=python3
+#
+# [56] 合并区间
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        intervals.sort(key=lambda x: (x[0],x[1]))
+        res = [intervals[0]]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= res[-1][1]:
+                res[-1][1] = max(intervals[i][1], res[-1][1])
+            else:
+                res.append(intervals[i])
+        return res
+# @lc code=end
+
